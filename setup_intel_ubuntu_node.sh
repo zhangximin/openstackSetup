97578f4ae5fc612c95fe36531ac7eabea578f875
ZhangXimin <zhangximin@gmail.com>
2015-03-25 16:39:28 +0800

    first commit without test.

---
+++ setup_intel_ubuntu_node.sh
@@ -0,0 +1,23 @@
+
+
+apt-get install -y git sudo
+
+groupadd stack
+
+useradd -g stack -s /bin/bash -d /opt/stack -m stack
+
+sudo echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+mkdir ~/.ssh; chmod 700 ~/.ssh
+
+cp ./authorized_keys ~/.ssh
+
+git clone https://git.openstack.org/openstack-dev/devstack
+
+cp ./nodes/local.conf ./nodes/local.sh ./devstack
+
+cd devstack
+
+./stack.sh
+
+
